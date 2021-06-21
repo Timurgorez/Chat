b1962325d99832ac37cf86405c9eea93be5f2f61
Timurgorez <timuumit2@gmail.com>
2021-06-21 14:26:04 +0300

    deleted Redux devtool

--- client/src/index.js
+++ client/src/index.js
@@ -17,7 +17,7 @@ const store = createStore(rootReducer, compose(
     // thunk,
     //runAPImiddleware
   ),
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 ));
 
 ReactDOM.render(
